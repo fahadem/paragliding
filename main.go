3952fde7a9db4791988846865567723c4684f1aa
Fahade <Vous@exemple.com>
2018-10-25 19:28:06 +0200

    commit main

--- main.go
+++ main.go
@@ -150,10 +150,11 @@ func trackHandler(w http.ResponseWriter, r *http.Request) {
 					fmt.Fprintln(w, "Use format id0 or id21 for exemple")
 				}
 			}
-			/*if strings.HasPrefix(parts[5],"field") {//if parts[5] != "" {
+			if strings.HasPrefix(parts[5],"field") {//if parts[5] != "" {
 
-				fmt.Fprintf(w,"Pilot: %s, gliderType: %s, gliderId: %s,track_length: %f, H_date: %s, track_src_url: %s", track.Pilot, track.GliderType,track.GliderID,track.Task.Distance(), track.Date.String(),url)
-			}*/
+				/*fmt.Fprintf(w,"Pilot: %s, gliderType: %s, gliderId: %s,track_length: %f, H_date: %s, track_src_url: %s", track.Pilot, track.GliderType,track.GliderID,track.Task.Distance(), track.Date.String(),url)*/
+					json.NewEncoder(w).Encode(track)
+			}
 		}
 	default:
 
